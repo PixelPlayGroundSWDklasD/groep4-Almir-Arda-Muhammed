713b77461b7130c97d5661140ff1d9e00b6247b5
Almir Maksuti <144669836+6036997@users.noreply.github.com>
2024-05-22 15:06:09 +0200

    connectie met database

--- lib/connection.php
+++ lib/connection.php
@@ -1,13 +1,33 @@
 <?php
 $servername = "localhost";
-$username = "pixel";
-$password = "12345";
+$username = "pixxel";
+$password = "1234567";
+$database = "pixelplayground1";
 
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+if (isset($_POST['verzenden'])) {
+  $userid = $_POST['userid'];
+  $wachtwoord = $_POST['wachtwoord'];
+  $wachtwoordconf = $_POST['wachtwoord_conf'];
+  $geheimevraag = $_POST['geheimevraag'];
+  $antw_vraag = $_POST['ant_geh_vraag'];
+}
+
+$sql = "INSERT INTO gebruikers (id, gebruikersnaam, wachtwoord, geheimevraag, antw_vraag) VALUES (NULL, '$userid', '$wachtwoord', '$geheimevraag', '$antw_vraag')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created succesfully";
+  echo "Welkom $userid";
+} else {
+  echo "Error: " . $sql . "<br>"  . $conn->error;
+}
+
+$conn->close();
 ?>
+
